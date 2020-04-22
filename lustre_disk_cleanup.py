53eeef58cfc181a6b5fcbb30dad1641c65d0f69b
Lei Pan <leipan@jpl.nasa.gov>
2020-04-22 06:14:55 -0700

    added cleanup() to remove files that are days old

--- lustre_disk_cleanup.py
+++ lustre_disk_cleanup.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
-from utils import (lustre_quota_info, find_cache_dir)
+from utils import find_cache_dir
+from test import lustre_quota_info
 import os, string, time
 import shutil
+import sys, getopt
+
+
+#----------------------------------------------------------------------
 
 # check every this many seconds
 check_interval = 600
@@ -15,6 +20,7 @@ def cleanup_old_jobs(work_path, userid, volume_root, threshold=10.):
     """If free disk space is below percent threshold, start cleaning out old jobs."""
 
     percent_free = lustre_quota_info(userid, volume_root)
+    logger.debug('percent_free: %s' % str(percent_free))
 
     if percent_free <= threshold:
         logger.info("Searching for old job dirs under %s to clean out to %02.2f%% free disk space." % (work_path, threshold))
@@ -44,6 +50,7 @@ def evict_localize_cache(work_path, userid, volume_root, threshold=10.):
     """If free disk space is below percent threshold, start evicting cache directories."""
 
     percent_free = lustre_quota_info(userid, volume_root)
+    logger.debug('percent_free: %s' % str(percent_free))
 
     if percent_free <= threshold:
         logger.info("Evicting cached dirs under %s to clean out to %02.2f%% free disk space." % (work_path, threshold))
@@ -65,18 +72,68 @@ def evict_localize_cache(work_path, userid, volume_root, threshold=10.):
     return percent_free
 
 
+
+
+
+def remove(path):
+    """
+    Remove the file or directory
+    """
+    if os.path.isdir(path):
+        try:
+            os.rmdir(path)
+        except OSError:
+            logger.info("Unable to remove folder: %s" % path)
+    else:
+        try:
+            if os.path.exists(path):
+                os.remove(path)
+        except OSError:
+            logger.info("Unable to remove file: %s" % path)
+
+
+
+
+def cleanup(number_of_days, path):
+    """
+    Removes files from the passed in path that are older than or equal 
+    to the number_of_days
+    """
+    time_in_secs = time.time() - (number_of_days * 24 * 60 * 60)
+    for root, dirs, files in os.walk(path, topdown=False):
+        for file_ in files:
+            full_path = os.path.join(root, file_)
+            ### logger.info('full_path: %s' % full_path)
+            try:
+              stat = os.stat(full_path)
+              if stat.st_mtime >= time_in_secs:
+                  remove(full_path)
+                  logger.info('removed file: %s' % full_path)
+            except FileNotFoundError:
+              islink = os.path.islink(full_path)
+              if islink:
+                os.unlink(full_path)
+                logger.info('unlinked full_path: %s' % full_path)
+
+        if not os.listdir(root):
+            remove(root)
+            logger.info('removed dir: %s' % root)
+
+
+
 def show_usage():
     print('Usage:\n')
     print('lustre_disk_cleanup.py --work_dir="/nobackupp12/lpan/worker/workdir/" --threshold=10.0 \n' )
 
 
-import sys, getopt
-
 def main(argv):
 
   # default values
   threshold = 10.0
-  work_path = '/nobackupp12/lpan/worker/workdir/'
+  threshold_dt = 5.0
+  days = 21
+  ### work_path = '/nobackupp12/lpan/worker/workdir/'
+  work_path = '/nobackupp12/lpan/worker/'
 
   try:
       opts, args = getopt.getopt(argv,"hd:t:",["work_dir=","threshold="])
@@ -110,11 +167,14 @@ def main(argv):
   logger.debug('threshold: %s' % threshold)
 
   while True:
+    # cleanup old files that are days old
+    cleanup(days, work_path)
+
     # cleanup done jobs if free disk space not met
     cleanup_old_jobs(work_path, userid, volume_root, float(threshold))
 
     # cleanup localized datasets/PGEs if free disk space not met
-    evict_localize_cache(work_path, userid, volume_root, float(threshold))
+    evict_localize_cache(work_path, userid, volume_root, float(threshold)+threshold_dt)
 
     time.sleep(check_interval)
 
